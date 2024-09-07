58f8a01766abfe7f2ec046c747df907ca3e2600c
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-07 18:58:57 -0500

    😺 implement the CustomButton

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import { Link } from 'expo-router';
+import { Redirect, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 
 const App = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode="contain" />
           <Image source={images.cards} className="max-w-[380px] w-full h-[300px] w" resizeMode="contain" />
           <View className="relative mt-5">
@@ -21,8 +23,10 @@ const App = () => {
           <Text className="text-sm fotn-pregular text-gray-100 mt-7 text-center">
             Where creativity meets innovation: embark on a journey of limitless exploration with shAIre
           </Text>
+          <CustomButton title="Continue with Email" handlePress={() => router.push('/sign-in')} containerStyles="w-full mt-7" />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor='#161622' style='light' />
     </SafeAreaView>
   );
 }
