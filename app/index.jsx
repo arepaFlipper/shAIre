7024b17c10c6131886194e9d0bb8a05c57d5d377
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-07 16:51:22 -0500

    😩 Dev home route

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 import { Link } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
 
 const App = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">shAIre!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'blue' }}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image source={images.logo} className="w-[130px] h-[84px]" resizeMode="contain" />
+          <Image source={images.cards} className="max-w-[380px] w-full h-[300px] w" resizeMode="contain" />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities with <Text className="text-secondary-200">shAIre</Text>
+            </Text>
+            <Image source={images.path} className="w-[136px] h-[15px] absolute -bottom-2 -right-8" resizeMode="contain" />
+          </View>
+
+          <Text className="text-sm fotn-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark on a journey of limitless exploration with shAIre
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
