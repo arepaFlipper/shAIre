4db8c9e104ef18200d4059203bffde7de9646538
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-07 08:45:58 -0500

    📄 add profile component in a link shape

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <Text>shAIre!</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
     </View>
   );
 }
