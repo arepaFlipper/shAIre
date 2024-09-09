ad48566178dfd30eb522252f63c67dac416eccf4
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-09 17:20:43 -0500

    🍀 play video in landscape

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -2,6 +2,7 @@ import { Image, Text, FlatList, TouchableOpacity, ImageBackground } from 'react-
 import React, { useState } from 'react'
 import * as Animatable from 'react-native-animatable';
 import { icons } from '../constants';
+import { Video, ResizeMode } from 'expo-av';
 
 const zoomIn = { 0: { scale: 0.8 }, 1: { scale: 1 } };
 
@@ -10,20 +11,46 @@ const zoomOut = { 0: { scale: 1 }, 1: { scale: 0.8 } };
 const TrendingItem = ({ item, activeItem }) => {
   const [play, setPlay] = useState(false)
   return (
-    <Animatable.View className="mr-5" animation={((activeItem === item.$id) ? zoomIn : zoomOut)}>
-      {(play) ?
-        (<Text>Playing...</Text>) :
-        (<TouchableOpacity className="relative justify-center items-center" activeOpacity={0.7} onPress={() => setPlay(true)}>
+    <Animatable.View
+      className="mr-5"
+      animation={activeItem === item.$id ? zoomIn : zoomOut}
+      duration={500}
+    >
+      {play ? (
+        <Video
+          source={{ uri: item.video }}
+          className="w-52 h-72 rounded-[33px] mt-3 bg-white/10"
+          resizeMode={ResizeMode.CONTAIN}
+          useNativeControls
+          shouldPlay
+          onPlaybackStatusUpdate={(status) => {
+            if (status.didJustFinish) {
+              setPlay(false);
+            }
+          }}
+        />
+      ) : (
+        <TouchableOpacity
+          className="relative flex justify-center items-center"
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+        >
           <ImageBackground
             source={{ uri: item.thumbnail }}
-            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black/40"
-            resizeMode='cover'
+            className="w-52 h-72 rounded-[33px] my-5 overflow-hidden shadow-lg shadow-black/40"
+            resizeMode="cover"
           />
-          <Image source={icons.play} className="w-12 h-12 absolute" resizeMode="contain" />
-        </TouchableOpacity>)}
+
+          <Image
+            source={icons.play}
+            className="w-12 h-12 absolute"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      )}
     </Animatable.View>
-  )
-}
+  );
+};
 
 const Trending = ({ posts }) => {
   const [activeItem, setActiveItem] = useState(posts[0]);
@@ -32,19 +59,21 @@ const Trending = ({ posts }) => {
     if (viewableItems.length > 0) {
       setActiveItem(viewableItems[0].key);
     }
-
   };
+
   return (
     <FlatList
       data={posts}
-      keyExtractor={(item) => item.$id}
-      renderItem={({ item }) => (<TrendingItem item={item} activeItem={activeItem} />)}
-      onViewableItemsChanged={viewableItemsChanged}
-      viewabilityCOnfig={{ itemVisiblePercentThreshold: 70 }}
-      contentOffset={{ x: 170 }}
       horizontal
+      keyExtractor={(item) => item.$id}
+      renderItem={({ item }) => {
+        return <TrendingItem activeItem={activeItem} item={item} />
+      }}
+      onViewableItemsChanged={viewableItemsChanged}
+      viewabilityConfig={{ itemVisiblePercentThreshold: 70 }}
+      contentOffset={{ x: 170 }}
     />
-  )
-}
+  );
+};
 
-export default Trending
+export default Trending;
