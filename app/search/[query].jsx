4577db452ab8399589cfb4344c0cee40c036d7d4
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-10 06:24:50 -0500

    🍟 use the SearchInput

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
+import { SafeAreaView, Text } from 'react-native'
 import React from 'react'
+import { useLocalSearchParams } from 'expo-router'
 
 const Search = () => {
+  const { query } = useLocalSearchParams();
   return (
-    <View>
-      <Text>[query]</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <Text className="text-3xl text-white">{query}</Text>
+    </SafeAreaView>
   )
 }
 
