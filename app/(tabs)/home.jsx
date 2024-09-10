e93d00a223b45db2bc4113455b73ea98e80da17a
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-10 14:47:09 -0500

    🥩 add videos by user

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,9 +9,11 @@ import EmptyState from '../../components/EmptyState';
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
 import useAppwrite from '../../lib/useAppwrite';
 import VideoCard from '../../components/VideoCard';
+import { useGlobalContext } from '../../context/GlobalProvider';
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
   const { data: latestPosts } = useAppwrite(getLatestPosts);
   const [refresh, setRefresh] = useState(false);
 
@@ -37,7 +39,7 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">Welcome Back</Text>
-                <Text className="text-2xl font-psemiblod text-white">arepa_flipper</Text>
+                <Text className="text-2xl font-psemiblod text-white">{user?.username || ""}</Text>
               </View>
               <View className="mt-1.5">
                 <Image source={images.logoSmall} className="w-9 h-10" resizeMode='contain' />
--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,53 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native';
+import React, { useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import SearchInput from '../../components/SearchInput';
+import EmptyState from '../../components/EmptyState';
+import { getUserPosts } from '../../lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
+import VideoCard from '../../components/VideoCard';
+import { useGlobalContext } from '../../context/GlobalProvider';
+import InfoBox from '../../components/InfoBox';
 
 const Profile = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts, refetch } = useAppwrite(() => getUserPosts(user.$id));
+  const logout = () => { }
+
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <SafeAreaView className="bg-primary border-2 border-red-500 h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => {
+          return item.$id
+        }}
+        renderItem={({ item }) => {
+          return <VideoCard item={item} />
+        }}
+        ListHeaderComponent={() => (
+          <View className="w-full justify-center items-center mt-6 mb-12 px-4">
+            <TouchacbleOpacity className="w-full justify-center items-center mt-6 mb-12 px-4">
+              <Image source={icons.logout} resizeMode="contain" className="w-6 h-6" onPress={logout} />
+              <View className="w-16 h-16 border border-secondary rounded-g justify-center items-center">
+                <Image source={{ uri: user?.avatar }} className="w-[90%] h-[90%] rounded-lg" resizeMode='cover' />
+                <InfoBox title={user?.username} containerStyles='mt-5' titlteStyles="text-lg" />
+
+                <View className="mt-5 flex-row">
+                  <InfoBox title={posts.length || 0} subtitle="Posts" containerStyles='mt-10' titleStyles="text-lg" />
+                  <InfoBox title="1.2k" containerStyles='Followers' titlteStyles="text-xl" />
+                </View>
+              </View>
+            </TouchacbleOpacity>
+          </View>
+        )}
+        ListEmptyComponent={() => {
+          return (
+            <EmptyState title="No Videos Found" subtitle="No videos found for this search query" />
+          )
+        }}
+      />
+    </SafeAreaView>
   )
 }
 
-export default Profile
-
-const styles = StyleSheet.create({})
+export default Profile;
